6e7cfd8d0bb9f8ecad20ace64215a0f189e870a4
Richard Hua <richyhua@gmail.com>
2020-03-20 19:11:00 -0700

    working graph constructor

--- my_graph.py
+++ my_graph.py
@@ -26,9 +26,32 @@ class MyGraph:
     def __init__(self, filename):
         """reads in the specification of a graph and creates a graph using an adjacency list representation. You may assume the graph is not empty and is a correct specification. E.g. each edge is represented by a pair of vertices between 1 and n. Note that the graph is not directed so each edge specified in the input file should appear on the adjacency list of each vertex of the two vertices associated with the edge.
         """
-        num_vertices, adj = self.init_vertices(filename)
-        self.num_vertices = num_vertices
-        self.adj = adj
+        self.is_bicolor = True
+        with open(filename) as infile:
+            for idx, line in enumerate(infile):
+                line = line.split()
+                if idx == 0:
+                    self.num_vertices = int(line[0])
+                    print('num vertices', self.num_vertices)
+                    self.vertices = [None] * (self.num_vertices + 1) #empty 0 idx
+                    for key in range(1, self.num_vertices + 1):
+                        self.vertices[key] = Vertex(key)
+                    # print(self.vertices)
+                if idx > 1: # connected vertices
+                    src = int(line[0])
+                    dest = int(line[1])
+                    print('src', src)
+                    print('dest', dest)
+                    if dest not in self.vertices[src].edges:
+                        self.vertices[src].edges.append(dest)
+                        self.vertices[src].num_edges += 1
+                    if src not in self.vertices[dest].edges:
+                        self.vertices[dest].edges.append(src)
+                        self.vertices[dest].num_edges += 1
+
+        # self.init_vertices(self.vertices)
+        # num_vertices, adj = self.init_vertices(filename)
+        # self.adj = adj
 
     def __eq__(self, other):
         return isinstance(other, MyGraph)\
@@ -37,41 +60,37 @@ class MyGraph:
         and (self.num_items == other.num_items)
 
     def __repr__(self):
-        return 'MyGraph(num_verts=%s, num_edges=%s, vlist=%s)'\
-        % (self.num_vertices, self.num_edges, self.vlist)
+        return 'MyGraph(num_verts=%s, Vertices=%s)'\
+        % (self.num_vertices, self.vertices)
 
     def get_conn_components(self, is_dfs=True):
         """returns a list of lists. For example, if there are three connected components then you will return a list of three lists. The order of the sub-lists is not important. However each sub-list will contain the vertices (in ascending order) in the connected component represented by that list. Each vertex is represented by an integer from 1 to n. If a vertex has no edges it will be in a connected component containing only itself. This function shall call either dfs or bfs method depending on the value of the argument is_dfs: i.e. do DFS if is_dfs=True, otherwise do BFS.
         """
-        pass
+        self.init_vertices(self.vertices)
 
-    def init_vertices(self, filename):
-        """initializes vertices
+    def init_vertices(self):
+        """set vertices attributes
         """
-        with open(filename) as file:
-            for i,line in enumerate(file):
-                if i > 2:
+        # for vertex in self.vertices:
 
-                for word in line:
-
-            line = file.readlines() # returns list containing all lines
-            num_vertices = int(line[0])
-            num_edges = int(line[1])
-            line = line[2:]
-            self.vlist = [None] * self.num_vertices
-            # self.bicolorable = bool(num_vertices - num_edges == 1)
-            for i,pair in enumerate(line): # i.e. (3, 7) : pair of connected vertices
-                pair = pair.split()
-                print(pair)
-                src = int(pair[0])
-                dest = int(pair[1])
-                if src not in self.vlist:
-                    self.vlist[i] = self.init_vertices(src)
-                if dest not in self.vlist[src].edges:
-                    self.vlist[src].edges.append(dest)
-        vert = Vertex(vertex)
-        # vert.add_edge(dest)
-        return vert
+        # line = file.readlines() # returns list containing all lines
+        # num_vertices = int(line[0])
+        # num_edges = int(line[1])
+        # line = line[2:]
+        # self.vlist = [None] * self.num_vertices
+        # # self.bicolorable = bool(num_vertices - num_edges == 1)
+        # for i,pair in enumerate(line): # i.e. (3, 7) : pair of connected vertices
+        #     pair = pair.split()
+        #     print(pair)
+        #     src = int(pair[0])
+        #     dest = int(pair[1])
+        #     if src not in self.vlist:
+        #         self.vlist[i] = self.init_vertices(src)
+        #     if dest not in self.vlist[src].edges:
+        #         self.vlist[src].edges.append(dest)
+        # vert = Vertex(vertex)
+        # # vert.add_edge(dest)
+        # return vert
 
     def dfs(self, vertex, component):
         """does dfs, finds connected components, and determines if the graph is bicolorable or not. If it is bicolorable, it is going to set the is_bicolor member variable to True, otherwise False. Takes two arguments, vertex (Vertex) and component (list). Returns a connected component as a list of vertex keys.
--- my_vertex.py
+++ my_vertex.py
@@ -21,13 +21,19 @@ class Vertex:
         predecessor (Vertex) : the predecessor
         color (str) : 'RED' or 'GREEN'
     """
-    def __init__(self, vertex):
+    def __init__(self, key):
         """reads in the specification of a graph and creates a graph using an adjacency list representation. You may assume the graph is not empty and is a correct specification. E.g. each edge is represented by a pair of vertices between 1 and n. Note that the graph is not directed so each edge specified in the input file should appear on the adjacency list of each vertex of the two vertices associated with the edge.
         """
-        self.key = vertex
+        self.key = key
         self.edges = [] # holds list of 2nd connected vertex
         self.num_edges = 0
+        self.discovered = False
+        self.color = None
 
+    def __repr__(self):
+        return 'Vertex(key=%s, num_edges=%s, edges=%s)'\
+        % (self.key, self.num_edges, self.edges)
+        
     def add_edge(self, dest):
         self.edges.append(v)
         self.num_edges += 1
